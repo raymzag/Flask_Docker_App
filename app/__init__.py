59b997332b54c6c5081602136498edb64a26a40f
Raymond Aung <raymond.psaung@gmail.com>
2022-07-22 10:15:13 +1000

    Add reproduction codes for unleash

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,30 @@
+import logging
+
 from flask import Flask
+from flask_unleash import Unleash
+
+logger = logging.getLogger()
 
 app = Flask(__name__)
 
+app.debug = True
+
+app.config['UNLEASH_URL'] = "https://app.unleash-hosted.com/demo/api"
+app.config['UNLEASH_CUSTOM_HEADERS'] = {'Authorization': '56907a2fa53c1d16101d509a10b78e36190b0f918d9f122d'}
+app.config['UNLEASH_APP_NAME'] = "flask-gunicorn-unleash"
+app.config['UNLEASH_ENVIRONMENT'] = 'staging'
+
+Unleash(app)
+
+h = logging.StreamHandler()
+fmt = logging.Formatter(
+    fmt='%(asctime)s %(levelname)s (%(name)s) %(message)s',
+    datefmt='%Y-%m-%dT%H:%M:%S'
+)
+h.setFormatter(fmt)
+logger.addHandler(h)
+logger.setLevel(logging.DEBUG)
+
 from app import views
+
 
--- app/views.py
+++ app/views.py
@@ -1,6 +1,14 @@
-from flask import render_template
+from flask import Blueprint, jsonify, render_template, current_app
 from app import app
 
 @app.route('/')
 def home():
     return render_template("index.html")
+
+
+@app.route('/test_unleash')
+def test_unleash():
+    unleash = current_app.extensions['Unleash']
+    print(f"Unleash check: {unleash.client.is_enabled('gunicorn')}")
+
+    return jsonify({'status': 'success', 'gunicorn': unleash.client.is_enabled('gunicorn')})
